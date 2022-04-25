45825af45d3ee74cdb909f7674fecc7c1a309eb9
Aaru-2111 <70098104+Aaru-2111@users.noreply.github.com>
2022-04-25 17:50:47 +0530

    setting folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Footer from './footer';
-import Header from './header';
-import Registration from './registration';
-import Query from './query';
-import Content from './contentp';
-import IMAGES from './images/img.js'
+import Footer from './Components/footer';
+import Header from './Components/header';
+import Registration from './Components/registration';
+import Query from './Components/query';
+import Content from './Components/contentp';
+import IMAGES from './Assets/img';
 function App() {
   return (
     
